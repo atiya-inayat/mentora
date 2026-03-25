55b55b87637d0c1df471a793917f26e24cadc497
atiya-inayat <atiyainayat869@gmail.com>
2026-03-25 18:36:42 +0500

    created user model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -10,18 +10,23 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
+      lowercase: true,
     },
     password: {
       type: String,
       required: true,
     },
     role: {
+      type: String,
       enum: ["mentee", "mentor", "admin"],
+      default: "mentee",
     },
   },
   {
     timestamps: true,
   },
 );
+
+userSchema.pre("save", async function (next) {});
 
 export default mongoose.model("User", userSchema);
