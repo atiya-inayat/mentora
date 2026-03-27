d784970e97cfac9fd70b9248e327067f03f13293
atiya-inayat <atiyainayat869@gmail.com>
2026-03-27 22:05:51 +0500

    added controller logic for login

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -36,3 +36,40 @@ export const register = async function (req, res) {
     res.status(500).json({ success: false, message: error.message });
   }
 };
+
+export const login = async function (req, res) {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const payload = {
+      sub: user._id, // Payload — the data you stored (like userId, role)
+
+      role: user.role,
+    };
+
+    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "7d" });
+
+    res.status(200).json({
+      success: true,
+      token: token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
