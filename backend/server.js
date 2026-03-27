d0a7c8832418560de6f2c84d2de626c072a21983
atiya-inayat <atiyainayat869@gmail.com>
2026-03-27 22:16:31 +0500

    added routes for auth

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import "dotenv/config"; // loads .env file first, before anything else
 import express from "express"; // default import — the express framework
 import cors from "cors"; // allows frontend to talk to your backend
 import { connectDB } from "./src/config/db.js"; // named import —  db function
+import router from "./src/routes/authRoutes.js";
 
 const app = express(); // creates your express app
 
@@ -10,8 +11,10 @@ app.use(express.json()); // middleware — lets you read JSON request bodies
 
 const PORT = process.env.PORT || 5000; // reads PORT from .env
 
-connectDB(); // connects to MongoDB
+app.use("/api/auth", router);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
