8dc4a6bdedaafab27b61b8e27167de70141fc10f
atiya-inayat <atiyainayat869@gmail.com>
2026-05-07 16:07:18 +0500

    created mentee dashboard

---
+++ frontend/src/app/dashboard/page.jsx
@@ -0,0 +1,25 @@
+"use client";
+import { useMyBookings } from "@/lib/hooks/useBookings";
+
+export default function MenteeDashboard() {
+  const { data, isLoading } = useMyBookings();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  const bookings = data?.data || []; // "If data exists, then access .data" OR "If bookings data exists, use it. Otherwise use empty array."
+
+  return (
+    <div>
+      <h1>My Bookings</h1>
+      {bookings.length === 0 && <p>No bookings yet</p>}
+      {bookings.map((booking) => (
+        <div key={booking._id}>
+          <p>Mentor: {booking.mentorId?.name}</p>
+          <p>Status: {booking.status}</p>
+          {/* Converts date into readable format.  */}
+          <p>Date: {new Date(booking.scheduledAt).toLocaleDateString()}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
